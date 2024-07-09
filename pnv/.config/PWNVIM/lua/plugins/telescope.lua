6c66283a8b4e913733c7a315bca8c2e8ce99332e
Mike <tetraedrgo@gmail.com>
2024-07-09 23:06:35 +0300

    add lazygit telescope update, and telescope recent files

--- pnv/.config/PWNVIM/lua/plugins/telescope.lua
+++ pnv/.config/PWNVIM/lua/plugins/telescope.lua
@@ -8,13 +8,51 @@ return {
       -- config goes here
     end,
   },
+
+  {
+    "kdheepak/lazygit.nvim",
+    cmd = {
+      "LazyGit",
+      "LazyGitConfig",
+      "LazyGitCurrentFile",
+      "LazyGitFilter",
+      "LazyGitFilterCurrentFile",
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { "<leader>lg", "<cmd>LazyGit<cr>", desc = "LazyGit Test" }
+    }
+  },
+
   {
     "nvim-telescope/telescope.nvim",
     tag = "0.1.5",
     dependencies = { 
       "nvim-lua/plenary.nvim",
       'andrew-george/telescope-themes',
+      "nvim-telescope/telescope-frecency.nvim",
+    }, 
+    keys = {
+      {
+      "<leader>fg",
+      -- require('telescope.buitin').live_grep,
+      '<cmd>Telescope live_grep<cr>',
+      desc = 'live grep',
+      mode = "n"
+      },
+      {
+        "<leader><leader>",
+        '<cmd>Telescope oldfiles<cr>',
+        desc = 'recent files',
+        mode = "n"
+      },
     },
+
     config = function()
       require("telescope").setup({
         defaults = {
@@ -42,13 +80,16 @@ return {
           },
         },
       })
-      local builtin = require("telescope.builtin")
+      -- local builtin = require("telescope.builtin")
       -- vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+      -- vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+      -- vim.keymap.set("n", "<leader><leader>", builtin.oldfiles, {})
+
+  
 
       require("telescope").load_extension("ui-select")
       require("telescope").load_extension('themes')
+      require("telescope").load_extension("frecency")
     end,
   },
 }
--- pnv/.config/PWNVIM/lua/plugins/user.lua
+++ pnv/.config/PWNVIM/lua/plugins/user.lua
@@ -28,7 +28,7 @@ return {
     "kelly-lin/ranger.nvim",
     config = function()
       require("ranger-nvim").setup({ replace_netrw = true })
-      vim.api.nvim_set_keymap("n", "<leader>ef", "", {
+      vim.api.nvim_set_keymap("n", "<leader>fe", "", {
         noremap = true,
         callback = function()
           require("ranger-nvim").open(true)
