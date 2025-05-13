2a6f2b730c7d33631432be308e05ecc551a3b988
Mike_Shevchuk <tetraedrgo@gmail.com>
2025-05-13 15:14:31 +0300

    updatate neotree color

---
+++ lz/.config/LazyVIM/lua/plugins/neo-tree.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons",
+    "MunifTanjim/nui.nvim",
+  },
+  config = function()
+    local commander = require("commander")
+    commander.add({
+      { keys = { "n", "<leader>e" }, cmd = "<cmd>Neotree toggle filesystem reveal right<cr>", desc = "Neotree" },
+      -- { keys={'n', '<leader>3'}, cmd='<cmd>cd %:h <CR>', desc='Change dir'},
+
+      {
+        keys = { "n", "<leader>3" },
+        cmd = function()
+          vim.cmd("cd %:h")
+          print("change dir to ", vim.fn.getcwd())
+        end,
+        desc = "Change dir",
+      },
+    })
+  end,
+}
